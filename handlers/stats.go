81e748b4110cc069d7c0a451829763f93bbdae0c
Piyush Verma <github@piyushverma.net>
2015-03-07 12:56:37 +0530

    Add the stats handler

---
+++ handlers/stats.go
@@ -0,0 +1,68 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/changer/khabar/db"
+
+	statsApi "github.com/changer/khabar/dbapi/stats"
+	"gopkg.in/simversity/gottp.v2"
+	gottp_utils "gopkg.in/simversity/gottp.v2/utils"
+)
+
+func ConcatenateErrors(errs *[]error) string {
+	var errString string
+	for i := 0; i < len(*errs); i++ {
+		errString += (*errs)[i].Error()
+		if (len(*errs) - i) > 1 {
+			errString += ","
+		}
+	}
+	return errString
+}
+
+type Stats struct {
+	gottp.BaseHandler
+}
+
+func (self *Stats) Get(request *gottp.Request) {
+	var args struct {
+		Organization string `json:"org"`
+		AppName      string `json:"app_name"`
+		User         string `json:"user" required:"true"`
+	}
+
+	request.ConvertArguments(&args)
+
+	err := gottp_utils.Validate(&args)
+	if len(*err) > 0 {
+		request.Raise(gottp.HttpError{http.StatusBadRequest, ConcatenateErrors(err)})
+		return
+	}
+
+	stats := statsApi.Get(db.Conn, args.User, args.AppName, args.Organization)
+
+	request.Write(stats)
+
+}
+
+func (self *Stats) Post(request *gottp.Request) {
+	var args struct {
+		Organization string `json:"org"`
+		AppName      string `json:"app_name"`
+		User         string `json:"user" required:"true"`
+	}
+
+	request.ConvertArguments(&args)
+
+	err := gottp_utils.Validate(&args)
+	if len(*err) > 0 {
+		request.Raise(gottp.HttpError{http.StatusBadRequest, ConcatenateErrors(err)})
+		return
+	}
+
+	statsApi.Save(db.Conn, args.User, args.AppName, args.Organization)
+
+	stats := statsApi.Get(db.Conn, args.User, args.AppName, args.Organization)
+	request.Write(stats)
+}
