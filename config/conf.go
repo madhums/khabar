6536f9bf8b71555bac19afb0886a17749fffb40a
Parth Desai <desaiparth08@gmail.com>
2015-03-17 18:27:30 +0530

    Default port will be at 8911

--- config/conf.go
+++ config/conf.go
@@ -5,15 +5,17 @@ import (
 )
 
 type config struct {
-	Gottp            conf.GottpSettings
+	Gottp  conf.GottpSettings
 	Khabar struct {
 		DBName               string
 		DBAddress            string
 		TranslationDirectory string
+		Debug                bool
 	}
 }
 
 func (self *config) MakeConfig(configPath string) {
+	self.Gottp.Listen = "127.0.0.1:8911"
 	if configPath != "" {
 		conf.MakeConfig(configPath, self)
 	}
