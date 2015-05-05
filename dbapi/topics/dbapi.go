6026aaf91dc6db8adb3273400e29a871763d9ad0
Parth Desai <desaiparth08@gmail.com>
2015-05-05 12:49:03 +0530

    Moving BLANK to models.go

--- dbapi/topics/dbapi.go
+++ dbapi/topics/dbapi.go
@@ -5,8 +5,6 @@ import (
 	"github.com/bulletind/khabar/utils"
 )
 
-const BLANK = ""
-
 func Update(user, org, topicName string, doc *utils.M) error {
 
 	return db.Conn.Update(db.TopicCollection,
@@ -31,9 +29,9 @@ func Delete(doc *utils.M) error {
 func ChannelAllowed(user, org, topicName, channel string) bool {
 	return db.Conn.Count(db.TopicCollection, utils.M{
 		"$or": []utils.M{
-			utils.M{"user": BLANK, "org": org},
-			utils.M{"user": BLANK, "org": BLANK},
-			utils.M{"user": user, "org": BLANK},
+			utils.M{"user": db.BLANK, "org": org},
+			utils.M{"user": db.BLANK, "org": db.BLANK},
+			utils.M{"user": user, "org": db.BLANK},
 			utils.M{"user": user, "org": org},
 		},
 		"ident":    topicName,
@@ -66,18 +64,18 @@ func findPerUser(user, org, topicName string) (topic *Topic, err error) {
 
 	topic, err = Get(user, org, topicName)
 	if err != nil {
-		topic, err = Get(user, BLANK, topicName)
+		topic, err = Get(user, db.BLANK, topicName)
 	}
 
 	return
 }
 
 func findPerOrgnaization(org, topicName string) (topic *Topic, err error) {
-	return Get(BLANK, org, topicName)
+	return Get(db.BLANK, org, topicName)
 }
 
 func findGlobal(topicName string) (topic *Topic, err error) {
-	return Get(BLANK, BLANK, topicName)
+	return Get(db.BLANK, db.BLANK, topicName)
 }
 
 func Find(user, org, topicName string) (topic *Topic, err error) {
