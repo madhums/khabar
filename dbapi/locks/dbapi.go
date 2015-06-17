764486b953413e95a22e4fa9fd703c64e3de3956
Parth Desai <desaiparth08@gmail.com>
2015-06-17 18:44:34 +0530

    Adding additional dbapi for locks

--- dbapi/locks/dbapi.go
+++ dbapi/locks/dbapi.go
@@ -3,6 +3,7 @@ package locks
 import (
 	"gopkg.in/bulletind/khabar.v1/db"
 	"gopkg.in/bulletind/khabar.v1/utils"
+	"gopkg.in/mgo.v2/bson"
 )
 
 func Insert(preference *db.Locks) string {
@@ -26,11 +27,32 @@ func GetAll(org string) []db.Locks {
 	return result
 }
 
-func IsLocked(org string, topic, channel string) bool {
+func Get(org, topic string, enabled bool) (error, *db.Locks) {
+	lock := new(db.Locks)
+
+	err := db.Conn.GetOne(db.LocksCollection, utils.M{
+		"org":     org,
+		"ident":   topic,
+		"enabled": enabled,
+	}, lock)
+
+	if err != nil {
+		return err, nil
+	}
+
+	return nil, lock
+}
+
+func Update(id bson.ObjectId, doc utils.M) error {
+	return db.Conn.Update(db.LocksCollection, utils.M{"_id": id}, doc)
+}
+
+func IsLocked(org string, topic, channel string, enabled bool) bool {
 	return db.Conn.Count(db.LocksCollection, utils.M{
 		"org":      org,
-		"topic":    topic,
+		"ident":    topic,
 		"channels": channel,
+		"enabled":  enabled,
 	}) != 0
 }
 
@@ -42,7 +64,7 @@ func GetAllEnabled(org string) []db.Locks {
 
 	db.Conn.Get(session, db.LocksCollection, utils.M{
 		"org":     org,
-		"enabled": "true",
+		"enabled": true,
 	}).All(&result)
 
 	return result
@@ -56,8 +78,40 @@ func GetAllDisabled(org string) []db.Locks {
 
 	db.Conn.Get(session, db.LocksCollection, utils.M{
 		"org":     org,
-		"enabled": "false",
+		"enabled": false,
 	}).All(&result)
 
 	return result
+}
+
+func AddChannel(topic, channel, organization string, enabled bool) error {
+
+	query := utils.M{
+		"org":     organization,
+		"ident":   topic,
+		"enabled": enabled,
+	}
+
+	spec := utils.M{"$addToSet": utils.M{"channels": channel}}
+
+	result := utils.M{}
+
+	_, err := db.Conn.FindAndUpdate(db.LocksCollection, query, spec, &result)
+	return err
+}
+
+func RemoveChannel(topic, channel, organization string, enabled bool) error {
+
+	query := utils.M{
+		"org":     organization,
+		"ident":   topic,
+		"enabled": enabled,
+	}
+
+	spec := utils.M{"$pull": utils.M{"channels": channel}}
+
+	result := utils.M{}
+
+	_, err := db.Conn.FindAndUpdate(db.LocksCollection, query, spec, &result)
+	return err
 }
