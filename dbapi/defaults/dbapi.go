091cec9a5d9bbc7aebbd9f3e1bda91e28fc5b428
Parth Desai <desaiparth08@gmail.com>
2015-06-17 18:45:24 +0530

    Adding extra dbapi for default

--- dbapi/defaults/dbapi.go
+++ dbapi/defaults/dbapi.go
@@ -3,6 +3,7 @@ package defaults
 import (
 	"gopkg.in/bulletind/khabar.v1/db"
 	"gopkg.in/bulletind/khabar.v1/utils"
+	"gopkg.in/mgo.v2/bson"
 )
 
 func Insert(preference *db.Defaults) string {
@@ -26,6 +27,36 @@ func GetAll(org string) []db.Defaults {
 	return result
 }
 
+func IsDefaultExists(org string, topic, channel string, enabled bool) bool {
+
+	return db.Conn.Count(db.DefaultsCollection, utils.M{
+		"org":      org,
+		"ident":    topic,
+		"channels": channel,
+		"enabled":  enabled,
+	}) != 0
+}
+
+func Get(org, topic string, enabled bool) (error, *db.Defaults) {
+	defaultPref := new(db.Defaults)
+
+	err := db.Conn.GetOne(db.DefaultsCollection, utils.M{
+		"org":     org,
+		"ident":   topic,
+		"enabled": enabled,
+	}, defaultPref)
+
+	if err != nil {
+		return err, nil
+	}
+
+	return nil, defaultPref
+}
+
+func Update(id bson.ObjectId, doc utils.M) error {
+	return db.Conn.Update(db.DefaultsCollection, utils.M{"_id": id}, doc)
+}
+
 func GetAllEnabled(org string) []db.Defaults {
 	session := db.Conn.Session.Copy()
 	defer session.Close()
@@ -34,7 +65,7 @@ func GetAllEnabled(org string) []db.Defaults {
 
 	db.Conn.Get(session, db.DefaultsCollection, utils.M{
 		"org":     org,
-		"enabled": "true",
+		"enabled": true,
 	}).All(&result)
 
 	return result
@@ -48,8 +79,40 @@ func GetAllDisabled(org string) []db.Defaults {
 
 	db.Conn.Get(session, db.DefaultsCollection, utils.M{
 		"org":     org,
-		"enabled": "false",
+		"enabled": false,
 	}).All(&result)
 
 	return result
+}
+
+func AddChannel(topic, channel, organization string, enabled bool) error {
+
+	query := utils.M{
+		"org":     organization,
+		"ident":   topic,
+		"enabled": enabled,
+	}
+
+	spec := utils.M{"$addToSet": utils.M{"channels": channel}}
+
+	result := utils.M{}
+
+	_, err := db.Conn.FindAndUpdate(db.DefaultsCollection, query, spec, &result)
+	return err
+}
+
+func RemoveChannel(topic, channel, organization string, enabled bool) error {
+
+	query := utils.M{
+		"org":     organization,
+		"ident":   topic,
+		"enabled": enabled,
+	}
+
+	spec := utils.M{"$pull": utils.M{"channels": channel}}
+
+	result := utils.M{}
+
+	_, err := db.Conn.FindAndUpdate(db.DefaultsCollection, query, spec, &result)
+	return err
 }
