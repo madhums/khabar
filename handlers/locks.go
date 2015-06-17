e6286f00c17e3387a04a726049ad071e5ab1f33a
Parth Desai <desaiparth08@gmail.com>
2015-06-17 18:55:01 +0530

    Add locks api

--- handlers/locks.go
+++ handlers/locks.go
@@ -1,19 +1,124 @@
 package handlers
 
-import "gopkg.in/simversity/gottp.v3"
+import (
+	"log"
+	"net/http"
 
-type Defaults struct {
+	"gopkg.in/bulletind/khabar.v1/core"
+	"gopkg.in/bulletind/khabar.v1/db"
+	"gopkg.in/bulletind/khabar.v1/dbapi/locks"
+	"gopkg.in/bulletind/khabar.v1/utils"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/simversity/gottp.v3"
+)
+
+type Locks struct {
 	gottp.BaseHandler
 }
 
-func (self *Defaults) Post(request *gottp.Request) {
+func (self *Locks) Post(request *gottp.Request) {
+	channelIdent := request.GetArgument("channel").(string)
+	topicIdent := request.GetArgument("ident").(string)
+
+	if !core.IsChannelAvailable(channelIdent) {
+		request.Raise(gottp.HttpError{
+			http.StatusBadRequest,
+			"Channel is not supported",
+		})
+
+		return
+	}
+
+	lock := new(db.Locks)
+	lock.PrepareSave()
+
+	lock.Channels = []string{channelIdent}
+
+	request.ConvertArguments(lock)
+	lock.Topic = topicIdent
+
+	if !utils.ValidateAndRaiseError(request, lock) {
+		return
+	}
+
+	if locks.IsLocked(lock.Organization, lock.Topic, channelIdent, lock.Enabled) {
+		request.Raise(gottp.HttpError{http.StatusConflict,
+			"Already Exists."})
+		return
+	}
+
+	if locks.IsLocked(lock.Organization, lock.Topic, channelIdent, !lock.Enabled) {
+		request.Raise(gottp.HttpError{http.StatusConflict,
+			"Already Set to Opposite. Please delete it and retry."})
+		return
+	}
+
+	err, existingObj := locks.Get(lock.Organization, lock.Topic, lock.Enabled)
+
+	if err != nil {
+		if err == mgo.ErrNotFound {
+			locks.Insert(lock)
+			request.Write(utils.R{StatusCode: http.StatusCreated, Data: nil,
+				Message: "Created"})
+			return
+		}
+		request.Raise(gottp.HttpError{http.StatusInternalServerError,
+			"Unable to complete db operation."})
+		return
+	}
+
+	err = locks.AddChannel(existingObj.Topic, channelIdent, existingObj.Organization, existingObj.Enabled)
+
+	if err != nil {
+		log.Println(err)
+		request.Raise(gottp.HttpError{http.StatusInternalServerError,
+			"Unable to complete db operation."})
+		return
+	}
+
+	request.Write(utils.R{
+		Data:       nil,
+		Message:    "true",
+		StatusCode: http.StatusNoContent,
+	})
+
+	return
 
 }
 
-func (self *Defaults) Get(request *gottp.Request) {
+func (self *Locks) Delete(request *gottp.Request) {
+	channelIdent := request.GetArgument("channel").(string)
+	topicIdent := request.GetArgument("ident").(string)
 
-}
+	if !core.IsChannelAvailable(channelIdent) {
+		request.Raise(gottp.HttpError{
+			http.StatusBadRequest,
+			"Channel is not supported",
+		})
 
-func (self *Defaults) Delete(request *gottp.Request) {
+		return
+	}
+
+	lock := new(db.Locks)
+	request.ConvertArguments(lock)
+	lock.Topic = topicIdent
+
+	if !locks.IsLocked(lock.Organization, lock.Topic, channelIdent, lock.Enabled) {
+		request.Raise(gottp.HttpError{http.StatusNotFound,
+			"Does not Exists."})
+		return
+	}
+
+	err := locks.RemoveChannel(lock.Topic, channelIdent, lock.Organization, lock.Enabled)
+
+	if err != nil {
+		request.Raise(gottp.HttpError{http.StatusInternalServerError,
+			"Unable to complete db operation."})
+		return
+	}
+
+	request.Write(utils.R{StatusCode: http.StatusNoContent, Data: nil,
+		Message: "NoContent."})
+	return
 
 }
