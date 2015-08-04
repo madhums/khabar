a7a44c95dc908236ca8d1c5fc71d0cba233f2d9a
Madhusudhan Srinivasa <madhums8@gmail.com>
2015-08-04 17:45:28 +0200

    fix tests

--- core/sender_test.go
+++ core/sender_test.go
@@ -9,10 +9,26 @@ import (
 const (
 	validName   = "incidentapp"
 	invalidName = "sheldon-cooper"
+	dbName      = "notifications_test"
 )
 
 func init() {
-	db.Conn = db.GetConn("notifications_testing", "localhost")
+	db.Conn = db.GetConn(dbName, "localhost")
+	setup()
+}
+
+func setup() {
+	var t *testing.T
+	available := db.AvailableTopic{
+		Ident:    "high_prio_log_incoming",
+		AppName:  validName,
+		Channels: []string{"email", "web", "push"},
+	}
+	available.PrepareSave()
+	err := db.Conn.Session.DB(dbName).C(db.AvailableTopicCollection).Insert(available)
+	if err != nil {
+		t.Error("Unable to setup db for testing")
+	}
 }
 
 func TestValidCategory(t *testing.T) {
@@ -25,6 +41,8 @@ func TestValidCategory(t *testing.T) {
 		t.Error(validName, "is not a valid category/app_name")
 		return
 	}
+
+	defer cleanup()
 }
 
 func TestGetParseKeys(t *testing.T) {
@@ -37,4 +55,10 @@ func TestGetParseKeys(t *testing.T) {
 	if _, ok := channelData["parse_rest_api_key"]; !ok {
 		t.Error("parse_rest_api_key is not set for category", validName)
 	}
+
+	defer cleanup()
+}
+
+func cleanup() {
+	db.Conn.Session.DB(dbName).DropDatabase()
 }
