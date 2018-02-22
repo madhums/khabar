c7a14e1356fb0e83c0e747be9aec46cb0a8c42a0
aderjaan <aderjaan.vandenberg@changer.nl>
2018-02-22 14:19:35 +0100

    don't bother to cleanup assets

--- db/cleaner.go
+++ db/cleaner.go
@@ -1,8 +1,6 @@
 package db
 
 import (
-	"regexp"
-	"strings"
 	"time"
 
 	"github.com/bulletind/khabar/utils"
@@ -13,8 +11,6 @@ import (
 func CleanupCollections() {
 	tables := []string{ProcessedCollection, SavedEmailCollection, SavedPushCollection, SentCollection}
 	rec := new(BaseModel)
-	sent := new(SentItem)
-	moireHost := utils.GetEnv("MOIRE_HOST", true)
 	dayToKeep := utils.EpochDate(time.Now().AddDate(0, 0, -30))
 
 	go func() {
@@ -30,18 +26,8 @@ func CleanupCollections() {
 				"_id": 1, "destination_uri": 1,
 			}).Limit(500).Sort("updated_on").Iter()
 
-			if table == SentCollection {
-				for cursor.Next(sent) {
-					ids = append(ids, sent.Id)
-					if len(moireHost) > 0 && strings.Contains(sent.DestinationUri, moireHost) {
-						re, _ := regexp.Compile("^(" + moireHost + "/assets/)(.*)([?].*)")
-						utils.DeleteFile(re.FindStringSubmatch(sent.DestinationUri)[2])
-					}
-				}
-			} else {
-				for cursor.Next(rec) {
-					ids = append(ids, rec.Id)
-				}
+			for cursor.Next(rec) {
+				ids = append(ids, rec.Id)
 			}
 
 			Conn.Delete(table, utils.M{
